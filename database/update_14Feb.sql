1fc7bb5d4bef55fc665195577cc994ac0e282b4e
Rahul Katariya <rkkatariya@gmail.com>
2017-02-14 12:56:19 +0530

    changed email log body to MEDIUMTEXT

---
+++ database/update_14Feb.sql
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  rahulkk
+ * Created: 14 Feb, 2017
+ */
+
+ALTER TABLE `sb_email_log` 
+CHANGE COLUMN `body` `body` MEDIUMTEXT NULL DEFAULT NULL ;
